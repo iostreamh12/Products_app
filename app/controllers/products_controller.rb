edbdb0bcb531bab673aece9ee892a6654600dcb6
Uziel <uac_h12@hotmail.com>
2014-10-06 18:55:35 +0000

    Products New

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,32 @@ class ProductsController < ApplicationController
   def edit
   end
 
+  def update
+    old_username = @user.username
+    new_params = user_params.dup
+    new_params[:username] = new_params[:username].strip
+    new_params[:email] = new_params[:email].strip
+    
+    @user.username = new_params[:username]
+    @user.email = new_params[:email]
+    @user.password = new_params[:password] if new_params[:password].strip.length > 0
+    @user.password_confirmation = new_params[:password_confirmation] if new_params[:password_confirmation].strip.length > 0
+    
+    if current_user.id != @user.id
+      @user.admin = new_params[:admin]=="0" ? false : true
+      @user.locked = new_params[:locked]=="0" ? false : true
+    end
+    
+    if @user.valid?
+      @user.skip_reconfirmation!
+      @user.save
+      redirect_to admin_users_path, notice: "#{@user.username} updated."
+    else
+      flash[:alert] = "#{old_username} couldn't be updated."
+      render :edit
+    end
+  end
+
   def create
     @product = Product.new(product_params)
     @product.save
